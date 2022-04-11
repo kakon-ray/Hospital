37ec21402434e73b8bc7b06eaf0f3f84f6e0e3d8
Kakon Ray <thisiskakonray@gmail.com>
2022-04-11 09:06:48 +0600

    pagebanner page style change

--- src/component/PageBanner.js/PageBanner.js
+++ src/component/PageBanner.js/PageBanner.js
@@ -3,26 +3,21 @@ import React from "react";
 export default function PageBanner({ page }) {
   const myStyle = {
     backgroundImage:
-      "url('https://media-cdn.tripadvisor.com/media/photo-s/16/d0/ed/49/the-img-hospital-is-a.jpg')",
-    height: "60vh",
-    marginTop: "-70px",
+      "url('https://andshop-react.netlify.app/static/media/common_bg.7b040441.png')",
+    height: "40vh",
     fontSize: "50px",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
+    marginTop: ".2%",
   };
-  const pageStyle = {
-    left: "45%",
-    top: "50%",
-  };
+
   return (
-    <div style={myStyle} className="position-relative">
-      <div
-        className="position-absolute top-50 page text-light"
-        style={pageStyle}
-      >
-        {" "}
-        <h1>{page}</h1>
-      </div>
+    <div
+      style={myStyle}
+      className="position-relative mb-5 d-flex justify-content-center align-items-center"
+    >
+      <h1 style={{ color: "#fff" }}>{page}</h1>
+      <div className="position-absolute page text-light pageStyle"> </div>
     </div>
   );
 }
