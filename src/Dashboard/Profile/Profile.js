f0165801a400bf9bb0075306dcec5f390a6bc0ea
Kakon Ray <thisiskakonray@gmail.com>
2022-05-29 23:59:50 +0600

    dashboard style

--- src/Dashboard/Profile/Profile.js
+++ src/Dashboard/Profile/Profile.js
@@ -44,59 +44,6 @@ const Profile = () => {
           alt=""
         />
       </Card.Header>
-      <Card.Body>
-        <div className="row">
-          <div className="col-md-6">
-            <Button variant="info" className="mb-3 w-100">
-              Add Your Personal Information
-            </Button>
-            <Card>
-              <Card.Img variant="top" src={userValue[0]?.img} />
-              <Card.Body className="text-center">
-                <h1>{userValue[0]?.title}</h1>
-                <h4>{userValue[0]?.department}</h4>
-                <p>{userValue[0]?.email}</p>
-                <p>{userValue[0]?.visit}</p>
-                <p>{userValue[0]?.time}</p>
-                <Card.Text>{userValue[0]?.des}</Card.Text>
-                <Button variant="dark" className="m-1">
-                  Update Profile
-                </Button>
-                <Button variant="dark" className="m-1">
-                  Delete Profile
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-6">
-            <Button variant="info" className="mb-3 w-100">
-              Create blog and help people
-            </Button>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="dark">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="dark">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      </Card.Body>
     </Card>
   );
 };
